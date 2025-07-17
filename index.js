1559b17abd27deffacdbeb1d5045db22f872b3b5
Marwan <marwanmamdouh159@gmail.com>
2025-07-18 00:46:19 +0300

    edit link front end

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 
 const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "https://center-kitchen-frontend.vercel.app",
-  ],
+  origin: ["http://localhost:3000", "https://center-kitchen-client.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
